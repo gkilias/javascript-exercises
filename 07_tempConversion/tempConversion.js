fed4bb9eaab8b4f132fc2e09cfe88879dd910de3
ilias <96296002+gkilias@users.noreply.github.com>
2023-12-06 16:38:59 +0200

    fixed formula for celcius conversion'

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
 const convertToCelsius = function(temperature) {
-  return (Math.round((temperature - 32) * (5/9)) * 10) / 10
+  return Math.round((temperature - 32) * (5/9) * 10) / 10
 };
 
 const convertToFahrenheit = function(temperature) {
-  return (Math.round((temperature * (9/5)) + 32) * 10) / 10
+  return Math.round(((temperature * 9) / 5 + 32) * 10) / 10
 };
 
 // Do not edit below this line
